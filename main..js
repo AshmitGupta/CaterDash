b83363b9b8d373ba8dc68d76fc5efe96eec4b672
AshmitGupta <67188586+AshmitGupta@users.noreply.github.com>
2024-06-16 11:53:55 -0700

    Added rest

--- main..js
+++ main..js
@@ -370,6 +370,9 @@ if (mainHeading) {
     } else if (mainHeadingText === 'Obanhmi') {
       updateContent('Obanhmi', 'Catering', 'Offering authentic Vietnamese banh mi sandwiches for catering, featuring fresh ingredients and bold flavors.');
       toggleDisplay(true);
+    } else if (mainHeadingText === 'Jo's Italian Deli') {
+      updateContent('Obanhmi', 'Catering', 'Experience authentic Italian cuisine with our catering service offering delectable pastas and artisanal sandwiches.');
+      toggleDisplay(true);
     } else {
         toggleDisplay(false);
     }
